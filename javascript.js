a4e5bd7e582650ce26d93a9701ed299feac6edbe
NeytanBelisario <111703255+NeytanBelisario@users.noreply.github.com>
2023-09-30 14:11:12 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,94 +1,78 @@
-// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
-// details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
+const inputPesquisa = document.querySelector('.inputPesquisa');
+const botaoPesquisa = document.querySelector('.botaoPesquisa');
+// const informacoesFilme = document.querySelector('.informacoesFilme')
+var poster = document.querySelector('.poster')
+var titulo = document.querySelector('.titulo')
+var tipo = document.querySelector('.tipo')
+var genero = document.querySelector('.genero')
+var data = document.querySelector('.data')
+var nota = document.querySelector('.nota')
+var atores = document.querySelector('.atores')
+var resumo = document.querySelector('.resumo')
+var duracao = document.querySelector('.duracao')
+var temporadas = document.querySelector('.temporadas')
 
-const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list');
-const resultGrid = document.getElementById('result-grid');
+function resultados(filmes){
+    fetch(`https://www.omdbapi.com/?s=${filmes}&page=1&apikey=ec2d782a`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Filme/série não encontrado`)
+        }
+        return response.json();
+    })
+    .then(function(informacoesFilmes){
+        console.log(informacoesFilmes)
+        var id = informacoesFilmes.Search[0].imdbID;
+        fetch(`http://www.omdbapi.com/?i=${id}&apikey=ec2d782a`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Filme/série não encontrado`)
+            }
+            return response.json();
+        })
+        .then(function(mostrarFilmes){
+            console.log(mostrarFilmes)
+            var posterFilme = mostrarFilmes.Poster
+            var atoresFilme = mostrarFilmes.Actors
+            var generoFilme = mostrarFilmes.Genre
+            var notaFilme = mostrarFilmes.imdbRating
+            var resumoFilme = mostrarFilmes.Plot
+            var dataFilme = mostrarFilmes.Released
+            var duracaoFilme = mostrarFilmes.Runtime
+            var tituloFilme = mostrarFilmes.Title
+            var tipoFilme = mostrarFilmes.Type
+            var temporadasSerie = mostrarFilmes.totalSeasons
+            var filmesPesquisa = document.querySelector('.filmesPesquisa')
 
-// load movies from API
-async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-    // console.log(data.Search);
-    if(data.Response == "True") displayMovieList(data.Search);
+            filmesPesquisa.style.display = "flex"
+            poster.innerHTML = `<img src="${posterFilme}"></img>`
+            atores.innerHTML = `<h4>${atoresFilme}</h4>`
+            genero.innerHTML = `<h4>${generoFilme}</h4>`
+            nota.innerHTML = `<h4>${notaFilme}</h4>`
+            resumo.innerHTML = `<h4>${resumoFilme}</h4>`
+            data.innerHTML = `<h4>${dataFilme}</h4>`
+            duracao.innerHTML = `<h4>${duracaoFilme}</h4>`
+            titulo.innerHTML = `<h2>${tituloFilme}</h2>`
+            tipo.innerHTML = `<h4>${tipoFilme}</h4>`
+            if(tipoFilme == 'series'){
+                temporadas.innerHTML = `<h4>${temporadasSerie}</h4>`
+            }
+            else{
+                temporadasSerie.style.display = "none";
+            }
+        })
+    })
 }
 
-function findMovies(){
-    let searchTerm = (movieSearchBox.value).trim();
-    if(searchTerm.length > 0){
-        searchList.classList.remove('hide-search-list');
-        loadMovies(searchTerm);
-    } else {
-        searchList.classList.add('hide-search-list');
+botaoPesquisa.addEventListener('onclick', function(){
+    resultados(inputPesquisa.value)
+})
+
+inputPesquisa.addEventListener('keypress', enter)
+
+function enter(event) {
+    key = event.keyCode
+    if (key === 13) {
+        resultados(inputPesquisa.value)
     }
 }
-
-function displayMovieList(movies){
-    searchList.innerHTML = "";
-    for(let idx = 0; idx < movies.length; idx++){
-        let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-        movieListItem.classList.add('search-list-item');
-        if(movies[idx].Poster != "N/A")
-            moviePoster = movies[idx].Poster;
-        else 
-            moviePoster = "image_not_found.png";
-
-        movieListItem.innerHTML = `
-        <div class = "search-item-thumbnail">
-            <img src = "${moviePoster}">
-        </div>
-        <div class = "search-item-info">
-            <h3>${movies[idx].Title}</h3>
-            <p>${movies[idx].Year}</p>
-        </div>
-        `;
-        searchList.appendChild(movieListItem);
-    }
-    loadMovieDetails();
-}
-
-function loadMovieDetails(){
-    const searchListMovies = searchList.querySelectorAll('.search-list-item');
-    searchListMovies.forEach(movie => {
-        movie.addEventListener('click', async () => {
-            // console.log(movie.dataset.id);
-            searchList.classList.add('hide-search-list');
-            movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`);
-            const movieDetails = await result.json();
-            // console.log(movieDetails);
-            displayMovieDetails(movieDetails);
-        });
-    });
-}
-
-function displayMovieDetails(details){
-    resultGrid.innerHTML = `
-    <div class = "movie-poster">
-        <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
-    </div>
-    <div class = "movie-info">
-        <h3 class = "movie-title">${details.Title}</h3>
-        <ul class = "movie-misc-info">
-            <li class = "year">Year: ${details.Year}</li>
-            <li class = "rated">Ratings: ${details.Rated}</li>
-            <li class = "released">Released: ${details.Released}</li>
-        </ul>
-        <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
-        <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
-        <p class = "actors"><b>Actors: </b>${details.Actors}</p>
-        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
-        <p class = "language"><b>Language:</b> ${details.Language}</p>
-        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
-    </div>
-    `;
-}
-
-
-window.addEventListener('click', (event) => {
-    if(event.target.className != "form-control"){
-        searchList.classList.add('hide-search-list');
-    }
-});
